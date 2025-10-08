746ebb24640cc2d4566b1afae171be3702f344ab
Lakhveer Singh <lakhveerdhot120@gmail.com>
2025-10-08 21:28:28 +0530

    Updated to new Web App URL

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
     // After deploying the script, you'll get a URL like:
     // https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec
     
-    const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw-T0cU0YCsZORxug5UNAZMYFIbHAPsYBpC4Q8Pf5EOvTqcA5yLOswYoM4qYq8uheJr/exec';
+    const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyxR3gsbs8sBIA-XQJncH6Jluuna4z7OFZtdpLJixKPYI35bUx35irD1Hf9GiS-gncL/exec';
     
     // Set as direct string for consistency (for index.html)
     window.API_BASE_URL = GOOGLE_APPS_SCRIPT_URL;
